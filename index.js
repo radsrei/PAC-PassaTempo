811f8f6fc87ad9323a666f2288b927f44c2ff405
Madu3304 <Mduda0722@gmail.com>
2024-11-01 21:14:45 -0300

    ajuste do banco

---
+++ index.js
@@ -0,0 +1,26 @@
+config ();
+
+
+
+
+const con = "./views/config/banco.js";
+
+const { Sequelize } = require('sequelize');
+
+const con = new Sequelize('gestao_voluntarios', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql', // ou 'postgres', 'sqlite', 'mariadb', 'mssql', dependendo do banco de dados
+});
+
+// Testa a conexão com o banco de dados
+con.authenticate()
+  .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso.'))
+  .catch((err) => console.error('Erro ao conectar com o banco de dados:', err));
+
+// Sincroniza os modelos com o banco de dados e inicia o servidor
+con.sync()
+  //.sync({ force: true }) // Use com cautela em ambiente de desenvolvimento
+  .then(() => {
+    app.listen(8080, () => console.log('Servidor rodando na porta 8080.'));
+  })
+  .catch((err) => console.error('Erro ao sincronizar com o banco de dados:', err));
