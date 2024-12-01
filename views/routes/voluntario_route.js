fa971acb4ce984ab39f3bbc18321c85cae8addb8
LuizFelipeSchroderMarcon <lschrodermarcon@gmail.com>
2024-12-01 20:53:32 -0300

    criando endpoint e rotas dos indicadores de eventos e voluntários

--- views/routes/voluntario_route.js
+++ views/routes/voluntario_route.js
@@ -8,6 +8,8 @@ router.post('/voluntario', voluntario.createVoluntario);
 router.put('/voluntario/:id', voluntario.updateVoluntario);
 router.delete('/voluntario/:id', voluntario.deleteVoluntario);
 router.get('/voluntario/top', voluntario.getTopVoluntarios);
+router.get('/voluntario/nome', voluntario.getVoluntariosDisponiveis);
+router.get('/voluntario/total', voluntario.getTotalVoluntarios);
 
 
 export { router };
